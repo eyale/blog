484bc52d78b6d111fdf07f6a244db9066357858f
antong <antong@netvertise.com>
2017-04-25 20:54:27 +0300

    adding the post action creator and functionality to post posts

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import { reduxForm } from 'redux-form'
+import { createPost } from '../actions'
 
 class PostsNew extends Component {
   render () {
+    const {
+      fields: { title, categories, content },
+      handleSubmit,
+      createPost
+    } = this.props
+
     return (
       <div>
         <div className='text-xs-right'>
           <Link to='/' className='btn btn-primary'>Home</Link>
         </div>
-        Create Form.
+        <form onSubmit={handleSubmit(createPost)}>
+          <h3>Create a New Post</h3>
+          <div className='form-group'>
+            <label htmlFor=''>Title</label>
+            <input
+              type="text"
+              className='form-control'
+              {...title} />
+          </div>
+          <div className='form-group'>
+            <label htmlFor=''>Categories</label>
+            <input
+              type="text"
+              className='form-control'
+              {...categories} />
+          </div>
+          <div className='form-group'>
+            <label htmlFor=''>Content</label>
+            <input
+              type="textarea"
+              className='form-control'
+              {...content} />
+          </div>
+          <button type='submit' className='btn btn-primary'>
+            Submit
+          </button>
+        </form>
       </div>
     )
   }
 }
 
-export default PostsNew
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew)
