2296b7d71e9bf944d5e8a816f10c4de52abf279c
antong <antong@netvertise.com>
2017-05-08 20:16:24 +0300

    getting the posts

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,9 @@
-import FETCH_POSTS from '../actions'
+import { FETCH_POSTS } from '../actions'
 const INITIAL_STATE = { all: [], posts: null }
 
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
     case FETCH_POSTS:
-      console.log(action)
       return { ...state, all: action.payload.data }
     default :
       return state
